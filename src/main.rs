6b6a5634740269c58acd8af3188e0551649f9a53
Iain Buckingham <eee@ibuckingham.org>
2022-02-22 15:54:59 +0000

    Hardcoded response by method and path
    use port 8080
    return 405 Method Not Allowed if not a GET request,
    return a hardcoded HTML response for /index and /about,
    otherwise return 404 Not Found

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,46 @@
+use hyper::service::{make_service_fn, service_fn};
+use hyper::{header, Body, Method, Request, Response, Server, StatusCode};
 use std::convert::Infallible;
 use std::net::SocketAddr;
-use hyper::{Body, Request, Response, Server};
-use hyper::service::{make_service_fn, service_fn};
 
-async fn hello_world(_req: Request<Body>) -> Result<Response<Body>, Infallible> {
-    Ok(Response::new("Hello, World".into()))
+async fn get_response(req: Request<Body>) -> hyper::http::Result<Response<Body>> {
+    // only allow GET methods
+    match (req.method(), req.uri().path()) {
+        (&Method::GET, path) => get_get_response(path),
+        _ => Response::builder()
+            .status(StatusCode::METHOD_NOT_ALLOWED)
+            .body(Body::empty()),
+    }
+}
+
+fn get_get_response(path: &str) -> hyper::http::Result<Response<Body>> {
+    match path {
+        "/" | "/index" => Response::builder()
+            .header(header::CONTENT_TYPE, "text/html; charset=UTF-8")
+            .body(Body::from(
+                "<!DOCTYPE html><title>Iain's Blog</title><p>index of posts",
+            )),
+        "/about" => Response::builder()
+            .header(header::CONTENT_TYPE, "text/html; charset=UTF-8")
+            .body(Body::from(
+                "<!DOCTYPE html><title>Iain's Blog</title><p>About this blog",
+            )),
+        _ => Response::builder()
+            .status(StatusCode::NOT_FOUND)
+            .body(Body::empty()),
+    }
 }
 
 #[tokio::main]
 async fn main() {
-    // We'll bind to 127.0.0.1:3000
-    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    // We'll bind to 127.0.0.1:8080
+    let addr = SocketAddr::from(([127, 0, 0, 1], 8080));
 
     // A `Service` is needed for every connection, so this
-    // creates one from our `hello_world` function.
+    // creates one
     let make_svc = make_service_fn(|_conn| async {
         // service_fn converts our function into a `Service`
-        Ok::<_, Infallible>(service_fn(hello_world))
+        Ok::<_, Infallible>(service_fn(get_response))
     });
 
     let server = Server::bind(&addr).serve(make_svc);
